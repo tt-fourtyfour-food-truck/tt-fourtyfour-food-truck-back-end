12b5bee85caeb221d0238c5ecfb4b1474eacaa53
andrewsohrabi <andrew.sohrabi@gmail.com>
2021-03-29 18:08:19 -0700

    more set-up

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 require('dotenv').config()
 
+const server = require('./api/server.js');
 
-const express = require('express')
-const server = express()
-server.use(express.json())
-const cors = require('cors')
-
-// on Heroku machine, an env variable is called "NODE_ENV" --< 'production'
-if (process.env.NODE_ENV === 'development') { 
-    const cors = require('cors')
-    server.use(cors())
-}
-
-server.get('/', (req,res)=> {
-    res.json({message:'api is up'})
-})
-
-const PORT = process.env.PORT || 4000
-
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-})
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
